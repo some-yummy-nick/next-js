a365d87c7855c6f096546295d2c0bcad5b84cf45
some.yummy.nick <some.yummy.nick@gmail.com>
2021-11-23 21:01:22 +0300

    Update link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ const MyApp = ({Component, pageProps}: AppProps) => (
     <Layout>
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet"/>
         </Head>
         <Component {...pageProps} />
